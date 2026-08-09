c3b6d0fe9efdbbb3a623cf2c57ccb313bbcee5ff
Daniel-tran1465 <justhuy1405@gmail.com>
2026-08-09 13:08:07 +0700

    Add Synchroniser module for UART_RX

---
+++ rtl/UART_RX/Sync.sv
@@ -0,0 +1,23 @@
+module Sync #(
+parameter bit idle_state
+)(
+input logic clk,
+input logic reset,
+input logic ASync,
+output logic Sync_out
+);
+
+logic [1:0] SR;
+
+always_ff@(posedge clk or posedge reset) begin
+if(reset)
+   SR <= {idle_state};
+else begin
+   SR[0] <= ASync;
+	SR[1] <= SR[0];
+	end
+end
+
+assign Sync_out = SR[1];
+
+endmodule
